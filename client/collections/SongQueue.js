40ea337b6cd6291cb6779850e0a55cf7b58efbc3
Hack Reactor Student <student@hackreactor.com>
2017-04-10 19:36:04 -0700

    Finished most of song queue specs

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,7 +3,37 @@ var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
 
+  // length: 2,
+
   initialize: function() {
-  }
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function() {
+      this.dequeue();
+    })
+
+
+  },
+
+
+
+  playFirst: function() {
+
+  },
+
+  dequeue: function (song) {
+    this.shift();
+    if (this.length >= 1) {
+      this.playFirst()
+    }
+  },
+
+  // remove: function(song) {
+
+  // }
 
 });
